f83d3c66825800707fff3ed952cdb15ceb56bb6b
Jun <Jun@ECUT.com>
2021-08-26 22:35:32 +0800

    hello commit

---
+++ src/main/java/Jun/hello.java
@@ -0,0 +1,11 @@
+package Jun;
+
+/**
+ * @author rookie
+ * @create 2021-08-26-22:18
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("阿萨德撒发");
+    }
+}
