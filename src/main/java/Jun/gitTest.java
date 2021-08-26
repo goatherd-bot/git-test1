e14825a6abf6132098c65d3fd60724c193de5ff8
Jun <Jun@ECUT.com>
2021-08-26 21:46:15 +0800

    pull commit

---
+++ src/main/java/Jun/gitTest.java
@@ -0,0 +1,18 @@
+package Jun;
+
+/**
+ * @author rookie
+ * @create 2021-08-26-21:45
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4 hot-fix");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+        System.out.println("push test!");
+        System.out.println("pull test!");
+    }
+}
